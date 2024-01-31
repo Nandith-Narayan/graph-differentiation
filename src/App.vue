1cce2135805c1f70fa87eeea7e03069f2c3f7587
Nandith-Narayan <nandith2ndemail@gmail.com>
2024-01-31 05:10:51 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ExpressionInput from './components/ExpressionInput.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ExpressionInput />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+body{
+  --bg-color1: #0F0F0F;
+  --bg-color2: #232D3F;
+  --fg-color1: #005B41;
+  --fg-color2: #008170;
+  background-color: var(--bg-color1);
 }
 </style>
---
+++ src/components/ExpressionInput.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+const raw_input = ref("x^2 + 4")
+</script>
+
+<template>
+  <div id="input-container">
+    <label for="raw_input">
+    <input id="raw_input" type="text" :value="raw_input"/>
+  </label>
+  </div>
+</template>
+
+<style scoped>
+  #input-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  label{
+    padding: 5em;
+  }
+  #raw_input{
+    background-color: transparent;
+    border-color: transparent;
+    color: var(--fg-color1);
+    font-size: xx-large;
+  }
+  #raw_input:focus{
+    color: var(--fg-color2);
+    outline: none;
+  }
+
+</style>
